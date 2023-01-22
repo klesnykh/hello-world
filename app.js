dd8caf6c0a8b13069acb5421bb860ce028b06d51
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-01-21 21:53:42 -0800

    lab8 done

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-function diseaseGreeting(){
+function greeting(){
     let month = prompt("What month were you born? (1-12):  ");
-    console.print(month);
     while (month < 0 || month > 12){
         month = prompt("Incorrect input, please only enter a number 1-12:  ");
     }
@@ -36,24 +35,30 @@ function diseaseGreeting(){
     }
 
     document.write('You were born in a month that gives you a higher likelyhood to develope ' + disease + '. Studies have shown that almost every birth month gives that person a higher likelyhood to develope a certain disease over their lifetime. Regardless of the disease you might end up getting, life is way better with a nicely remodeled kitchen! ');
-}
 
-function getName(){
     let n = prompt('What is your name?');
     while (n == ''){
         n = prompt('I promise it\'ll be worth it! Please, what is your name?');
     }
-    return n;
-}
 
-function getRemodelPrice(){
     let price = prompt('If you WERE to remodel anything in your current home, how much would you want to spend? (0-200000):  ');
     while (price < 1 || price > 200001 || price == ''){
         price = prompt('Seriously! We\'re here to help. You\'re here because you\'re looking into remodeling a part of your home. Just give us an approximate amount you want to spend:  ');
     }
-    return price;
+
+    document.write(n + ', we can definitely try to get you a remodel for $' + price + '. If you\'re serious about figuring out how to get yourself a fresh remodel based on your own terms and expectations, give us a call! Our contact info is on the bottom of the page');
 }
 
-function kitchenGreeting(){
-    document.write(getName() + ', we can definitely try to get you a new kitchen for $' + getRemodelPrice() + '. If you\'re serious about figuring out how to get yourself a new kitchen based on your own terms and expectations, give us a call! Our contact info is on the bottom of the page');
+function siteRating(){
+    let rating = prompt('How would you rate our website so far? (1-5 stars):  ');
+    while (rating < 1 || rating > 5){
+        month = prompt("Incorrect input, please only enter a number 1-5:  ");
+    }
+
+    let myImg = "<img src='star.jpg' alt='' />";
+
+    for (let i = 1; i <= rating; i++){
+    document.write(myImg);
+    }
+    document.write('Thank you for your feedback!!');
 }
