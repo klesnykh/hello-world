b924e20ced8cb7c7ec2209510e182fbe5f81fdfd
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-01-21 19:25:39 -0800

    first try with class 08 loops

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function greeting(){
-    let month = prompt("What month were you born? (1-12)");
-        if (month == ''){
-            month = prompt('To choose a month, please select a number from 1 to 12');
-        }
+function diseaseGreeting(){
+    let month = prompt("What month were you born? (1-12):  ");
+    while (month<0 || month>12){
+        let m = prompt("Incorrect input, please only enter a number 1-12:  ");
+    }
 
     let disease;
 
@@ -35,14 +35,24 @@ function greeting(){
     }
 
     document.write('You were born in a month that gives you a higher likelyhood to develope ' + disease + '. Studies have shown that almost every birth month gives that person a higher likelyhood to develope a certain disease over their lifetime. Regardless of the disease you might end up getting, life is way better with a nicely remodeled kitchen! ');
+}
 
+function getName(){
     let n = prompt('What is your name?');
-        if (n == ''){
-            n = prompt('I promise it\'ll be worth it! Please, what is your name?');
-        }
-    let price = prompt('If you WERE to remodel your kitchen, how much would you want to spend? (0-120000)');
-        if (price == ''){
-            price = prompt('Honestly, at this point we\'ll take any number');
-        }
-    document.write(n + ', we can definitely try to get you a new kitchen for $' + price + '. If you\'re serious about figuring out how to get yourself a new kitchen based on your own terms and expectations, give us a call! Our contact info is on the bottom of the page');
+    while (n == ''){
+        n = prompt('I promise it\'ll be worth it! Please, what is your name?');
+    }
+    return n;
+}
+
+function getRemodelPrice(){
+    let price = prompt('If you WERE to remodel anything in your current home, how much would you want to spend? (0-200000):  ');
+    while (price < 1 || price > 200001 || price == ''){
+        price = prompt('Seriously! We\'re here to help. You\'re here because you\'re looking into remodeling a part of your home. Just give us an approximate amount you want to spend:  ');
+    }
+    return price;
+}
+
+function kitchenGreeting(){
+    document.write(getName() + ', we can definitely try to get you a new kitchen for $' + getRemodelPrice() + '. If you\'re serious about figuring out how to get yourself a new kitchen based on your own terms and expectations, give us a call! Our contact info is on the bottom of the page');
 }
